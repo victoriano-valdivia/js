30092281ad1d55462f5a467d10d0b3f51cd3b6e9
victoriano-valdivia <107598309+victoriano-valdivia@users.noreply.github.com>
2022-08-25 20:47:52 -0300

    convertidor

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 function convertir(){
-      var valore = parseInt(document.getElementById("valor").Value);
-      var resultado = 0;
-      var dolar = 290;
-      var euro = 295;
+      let valore = document.getElementById("valor").value;
+      let resultado = 0;
+      let dolar = 290;
+      let euro = 295;
+
       if (document.getElementById("uno").checked){
-            resultado = valore / dolar;
-            alert("el cambio de pesos a dolar es:" + resultado);
+            resultado = valore * dolar;
+            alert("el cambio de pesos a dolar es: $" + resultado);
       }
-else if (document.getElementById("dos").checked){
-      resultado = valore / euro;
-      alert("el cambio de pesos a euros es:" + resultado);
-}
-else{
+      else if (document.getElementById("dos").checked){
+            resultado = valore * euro;
+            alert("el cambio de pesos a euros es: $" + resultado);
+      }
+      else{
       alert("tienes que completar todos los requeriminetos")}
 }
